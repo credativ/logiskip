2db7579884c8874507ea2ddc1422235f9f428610
Dominik George <dominik.george@credativ.de>
2021-04-06 18:57:29 +0200

    Fix some mistakes in load base

--- logiskip/load.py
+++ logiskip/load.py
@@ -1,6 +1,6 @@
 """logiskip's base code for loads"""
 
-from typing import Optional, Sequence
+from typing import Optional, Sequence, Union
 
 from semantic_version import SimpleSpec, Version
 from sqlalchemy.engine.base import Engine
@@ -10,7 +10,7 @@ from sqlalchemy.orm import Session
 class LoadRegistry:
     """Registry object that collects and finds available loads"""
 
-    _loads: dict[str, Dict[str, "BaseLoad"]]
+    _loads: dict[str, dict[str, "BaseLoad"]]
 
     def __init__(self):
         self._loads = {}
@@ -22,7 +22,7 @@ class LoadRegistry:
     def find(self, name: str, version: Union[Version, str]) -> Optional["BaseLoad"]:
         """Find a load matching the given name and version"""
         if isinstance(version, str):
-            version = Verison(version)
+            version = Version(version)
 
         for constraint, load_class in self._loads.get(name, {}).items():
             spec = SimpleSpec(constraint)
@@ -37,15 +37,16 @@ load_registry = LoadRegistry()
 class BaseLoad:
     """Base class for logiskip load definitions"""
 
-    @classmethod
-    def __init__subclass__(cls, name: Optional[str] = None, version_constraint: str = "*"):
+    def __init_subclass__(cls, /, name: Optional[str] = None, version_constraint: str = "*", **kwargs):
         """Register a load subclass so it can be found by name and version constraint"""
-        if load_name is None:
+        super().__init_subclass__(**kwargs)
+
+        if name is None:
             # Guess load name from class module
-            if cls.__module__.__name__.startswith("logiskip.loads."):
-                load_name = cls.__module__.__name__.split(".")[2]
+            if cls.__module__.startswith("logiskip.loads."):
+                name = cls.__module__.split(".")[2]
             else:
-                raise ValueError("No load_name passed and load not in logiskip.loads namespace.")
+                raise TypeError("No load name passed and load not in logiskip.loads namespace.")
 
         load_registry.register(name, version_constraint, cls)
 
