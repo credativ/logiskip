a30cc8f83c21174e39aad79ff119ea4d0e3efc92
Dominik George <dominik.george@credativ.de>
2021-04-06 14:43:39 +0200

    Initialise load with automaps

--- logiskip/load.py
+++ logiskip/load.py
@@ -1,7 +1,20 @@
 """logiskip's base code for loads"""
 
+from sqlalchemy.engine.base import Engine
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+
 
 class BaseLoad:
-    @classmethod
+    def __init__(self, src: Engine, dest: Engine):
+        self.src_engine = src
+        self.src_base = automap_base()
+        self.src_base.prepare(self.src_engine, reflect=True)
+
+        self.dest_engine = dest
+        self.dest_base = automap_base()
+        self.dest_base.prepate(self.dest_engine, reflect=True)
+
+    @staticmethod
     def find_load(name: str, version: str) -> "BaseLoad":
         pass
