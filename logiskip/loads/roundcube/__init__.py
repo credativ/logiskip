1280365e76b8c8e689ebf5226d7f75edb2329573
Dominik George <dominik.george@credativ.de>
2021-04-07 11:26:01 +0200

    Finalise roundcube load

--- logiskip/loads/roundcube/__init__.py
+++ logiskip/loads/roundcube/__init__.py
@@ -4,4 +4,10 @@ from logiskip.load import BaseLoad
 
 
 class RoundcubeLoad(BaseLoad, name="roundcube", version_constraint="==1.4.1"):
+    # The "system" table is pre-fille dwith the schema version
     default_tables = {"system": None}
+
+    # The "dictionary" table has an "id" field in MySQL
+    # Note from schema: redundant, for compat. with Galera Cluster
+    # No other dialects have the field, so we can shorten it as default
+    default_fields_dictionary = {"id": None}
